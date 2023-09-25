4a2d527be315aabca9f0c644ffc26685c09f0563
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-09-25 18:02:18 -0300

    Update all products route
    update route

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,86 @@
-import { PrismaClient } from "@prisma/client"
-import cors from "cors"
-import dotenv from "dotenv"
-import express, { Express, Request, Response } from "express"
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
-import { CustomerData } from "./interfaces/CustomerData"
-import { PaymentData } from "./interfaces/PaymentData"
-import { ProductData } from "./interfaces/ProductData"
+import { CustomerData } from "./interfaces/CustomerData";
+import { PaymentData } from "./interfaces/PaymentData";
+import { ProductData } from "./interfaces/ProductData";
 
-import CheckoutService from "./services/CheckoutService"
+import CheckoutService from "./services/CheckoutService";
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT || 5000
-const prisma = new PrismaClient()
+const app: Express = express();
+const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  const { message } = req.body
+  const { message } = req.body;
 
-  if (!message) return res.status(400).send({ error: "Message is required" })
+  if (!message) return res.status(400).send({ error: "Message is required" });
 
-  res.send({ message })
-})
+  res.send({ message });
+});
 
 app.get("/products", async (req: Request, res: Response) => {
-  const { product } = req.query
+  const { product } = req.query;
 
-  if (!product) return res.status(400).send({ error: "Product is required" })
-
-  // SELECT * FROM Product WHERE product = 'drink'
-  const products = await prisma.product.findMany({
-    where: {
-      product: {
-        equals: product as string,
+  // Verifica se o parâmetro "product" está presente na consulta
+  if (product) {
+    const products = await prisma.product.findMany({
+      where: {
+        product: {
+          equals: product as string,
+        },
       },
-    },
-  })
+    });
+    return res.send(products);
+  }
 
-  res.send(products)
-})
+  // Se o parâmetro "product" não estiver presente, retorna todos os produtos
+  const allProducts = await prisma.product.findMany();
+  res.send(allProducts);
+});
 
 app.get("/orders/:id", async (req: Request, res: Response) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   const order = await prisma.order.findUnique({
     where: {
       id: +id,
     },
     include: { customer: true, orderItems: { include: { product: true } } },
-  })
+  });
 
-  if (!order) return res.status(404).send({ error: "Order not found" })
+  if (!order) return res.status(404).send({ error: "Order not found" });
 
-  res.send(order)
-})
+  res.send(order);
+});
 
 interface CheckoutRequest extends Request {
   body: {
-    cart: ProductData[]
-    customer: CustomerData
-    payment: PaymentData
-  }
+    cart: ProductData[];
+    customer: CustomerData;
+    payment: PaymentData;
+  };
 }
 
 app.post("/checkout", async (req: CheckoutRequest, res: Response) => {
-  const { cart, customer, payment } = req.body
+  const { cart, customer, payment } = req.body;
 
   const orderCreated = await new CheckoutService().process(
     cart,
     customer,
     payment
-  )
+  );
 
-  res.send(orderCreated)
-})
+  res.send(orderCreated);
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
